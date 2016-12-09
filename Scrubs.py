eb7f5f1108ba88645303a98d61ea498b3137df6a
Gary Perez <gary@eringary.com>
2016-12-08 21:05:55 -0500

    Add links to scrubs.

--- Scrubs.py
+++ Scrubs.py
@@ -9,4 +9,14 @@ class Scrubs (Template_Authenticated):
     def writeContent(self):
         wr = self.writeln
         req = self.request()
-        wr(str(req))
+        IS_DIP = is_diplomate(req)
+        if IS_DIP:
+            wr('<h1>ACVAA Scrubs</h1>')
+            wr('<p>You can buy ACVAA scrubs from <a href="http://www.scrubsandbeyond.com/">Scrubs & Beyond</a>.</p>')
+
+
+
+
+        else:
+            wr('<h1>Sorry.</h1><p>Scrubs are only available to ACVAA Diplomates.</p>')
+
