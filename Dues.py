9f00c34b2ef4fb73238966ee9c9ceed8d52f8dff
Gary Perez <gary.perez@asti-usa.com>
2017-12-12 14:19:25 -0500

    Update content of the Dues page, per Lynne.

--- Dues.py
+++ Dues.py
@@ -33,26 +33,41 @@ You can pay dues with your credit card using PayPal.
         wr('''
 
 <P><b>Amount</b>: <b>$450</b></P>
-<P><b>Due</b>: January 1&ndash;April 1 (see the bylaws below for more info)</P>
+<P><b>Due</b>: January&ndash;March (see the bylaws below)</P>
 <P><b>Methods of Payment</b>:
-<ol><li> Check made out to ACVAA and mailed to the ACVAA Executive Secretary.
-<li> Credit Card by communicating the Visa or MasterCard numbers and expiration date to the Executive Secretary. <b>Please do not email CC info; it is a security risk!</b>
-<li> PayPal by clicking on the "Make Payment" button on this page to pay with a credit card, or an existing PayPal account you may have.
+<ol><li> Check made out the ACVAA and mailed to the ACVAA Executive Secretary
+<li> PayPal by clicking on the "make a payment" button on this page to pay with credit card or an existing PayPal account you may have.
+<li> If any questions about payment, please contact the ACVAA Secretary at <a href="mailto:execdir@acvaa.org">execdir@acvaa.org</a>
 </ol>
 </P>
 <P><b>Executive Secretary</b>: %s: %s</P>
 <blockquote>
-<h2>ACVAA Bylaws, Article VI - Dues</h2>
+<h2>ACVAA Bylaws, Article VI
+<br>
+Dues</h2>
 <P>
-<u>Section 1</u> &mdash; Annual dues for members of the College become <u>payable January 1 of each calendar year</u>. Any proposed change in the amount shall be recommended by the Board of Directors and approved by a majority vote of the attending Diplomates at the annual business meeting. A written notice of the proposed change must be sent to all Diplomates thirty days prior to the annual business meeting. 
+<u>Section 1</u> &mdash; Annual dues for members of the College become payable on or before January 1 of each calendar year. Any proposed change in the amount shall be recommended by the Board of Directors and approved by a majority vote of the attending Diplomates at the annual business meeting. A written notice of the proposed change must be sent to all Diplomates thirty days prior to the annual business meeting.
 </P>
 <P>
-<u>Section 2</u> &mdash; A notice of pending delinquency will be sent to members in arrears on <u>April 1</u>. Dues are delinquent <u>July 1</u> and, <u>if not paid by December 31 of the same year, membership in the College will become inactive</u>, but only after notification of the diplomate in question by registered mail, return receipt requested. A <u>fine of $50</u> will be added to annual dues paid <u>after October</u> of the same year.
+<u>Section 2</u> &mdash; Email reminders of the date for annual dues payment will be sent by the Executive Secretary/Administrator in January, February, and March of that year. An email notice of pending delinquency will be sent to individual members who have not paid the current year's annual dues by April 1. Dues are considered delinquent July 1, and membership in the College will become inactive, but only after notification of the diplomate in question by registered mail, return receipt requested. If the diplomate cannot be reached or response received by registered mail has failed alternate methods of contact including attempts by phone at the last known contact number should be made before the diplomate is placed in inactive status. It is a diplomate's responsibility to maintain current contact information with the Executive Secretary/Administrator.  A fine of $50 will be added to annual dues paid after April 1 of the same year.
+</P>
+
+<h2>Constitution Article V
+<br>
+Membership and Qualifications</h2>
+<P>
+Section 2,e<br>
+Inactive Diplomate<br>
+e.<br>
 </P>
 <P>
-<u>Section 3</u> &mdash; Reactivation of membership will require petition to the Executive Secretary including payment of all delinquent dues and fines and approval by a majority of the Board of Directors.
+A Diplomate will be deemed inactive upon non-payment of annual dues to the ACVAA. Dues are considered delinquent if not paid by July 1 of that calendar year. 
 </P>
 </blockquote>
+
+<P>
+See the ACVAA Constitution and Bylaws for more description of "inactive status". 
+</P>
 
 <P>
 Please also consider a donation to the <a href="http://acvaa.org/Foundation/">ACVAA Foundation</a>. Contributions of any amount are welcome and can be made by check, Visa or MasterCard as above or by Authorize.net and PayPal through the "Donate" buttons on the <a href="http://acvaa.org/Foundation/">ACVAA Foundation page</a> of this website.
