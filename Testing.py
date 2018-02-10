a1d34e9899c5c06aade3e496eb585b458b8fa9a9
Gary Perez <gary.perez@asti-usa.com>
2018-02-10 17:03:11 -0500

    Testing button.

--- Testing.py
+++ Testing.py
@@ -6,52 +6,16 @@ from z_docmgmt import get_all_possible_categories, MASTER_ROLES_LIST, \
 class Testing (Template_Authenticated):
 
     def title(self):
-        return 'Documents'
+        return 'Testing'
 
     def writeContent(self):
         wr = self.writeln
-        ISA = is_site_admin(self.request())
-        if ISA:
-            form = self.request().fields()
-            if self.request()._environ.get('REQUEST_METHOD') == 'POST':
-                ERRORS = []
-                if not form.get('title'):
-                    ERRORS.append('You must provide a title for this document.')
-                if not form.get('role'):
-                    ERRORS.append('You must allow access to one or more roles.')
-                if not form.get('category'):
-                    ERRORS.append('You must select one or more categories for this document.')
+        wr('''
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="NDCABTKMY694A">
+<input type="image" src="http://www.acvaa.org/g/btn_payduesCC_LG.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
 
-                fobj = form.get('datafile')
-                try:
-                    filename = fobj.filename
-                except:
-                    ERRORS.append('You must select a file on your computer to upload.')
-
-                if ERRORS:
-                    self.render_form_error('Upload Error', '<br>'.join(ERRORS))
-
-                else:
-#                    wr(form)
-                    save_doc_file(form)
-                    self.response().sendRedirect('DM_Index')
-
-
-            else:
-                wr('<h2>Add a Document</h2>')
-                wr('<p>All form fields are required.</p>')
-                wr('<form method="POST" action="Testing" enctype="multipart/form-data">')
-                wr('<table>')
-                wr('<tr><td>Title:<td><input type="text" name="title" value=""><br />')
-                wr('<tr><td>Categories:<br><small>(pick at least one)</small><td>')
-                for cat in get_all_possible_categories():
-                    wr('<label for="%s"><input type="checkbox" name="category" value="%s" id="%s"> %s</label><br>' % (cat, cat, cat, cat))
-                wr('<hr>')
-                wr('<td><a href="DM_Edit_Categories" class="btn btn-default btn-sm"><i class="fa fa-pencil">&nbsp;</i>Edit Categories</a>')
-                wr('<tr><td>Allowed access:<br><small>(pick at least one)</small><td>')
-                for role in MASTER_ROLES_LIST:
-                    wr('<label for="%s"><input type="checkbox" name="role" value="%s" id="%s"> %s</label><br>' % (role, role, role, role))
-                wr('<tr><td>File:<td><input type="file" name="datafile"><br />')
-                wr('<tr><td><td><input type="submit" value="Upload">')
-                wr('</table>')
-                wr('</form>')
+        ''')
